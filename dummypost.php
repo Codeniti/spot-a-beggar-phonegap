85ab2a1094d95f107fce31998954eb170b53867b
ashriths <ashrith.sheshan@gmail.com>
2014-03-01 21:24:36 +0530

    Chnaged snapshots

---
+++ dummypost.php
@@ -0,0 +1,36 @@
+<html>
+<body>
+Form for getPerson.php
+<form method="post" action="getperson.php">
+ <input placeholder="lat" name="lat" type="text"/>
+  <input placeholder="long" name="long" type="text"/>
+  
+	<input type="submit">
+</form>
+
+Form for addperson.php
+<form method="post" action="http://www.oth1.com/codeniti/addperson.php">
+ <input placeholder="pic" name="pic" type="text"/>
+  <input placeholder="lat" name="lat" type="text"/>
+    <input placeholder="long" name="long" type="text"/>
+	 <input placeholder="rating" name="rating" type="text"/>
+	<input type="submit">
+</form>
+
+Form for authenticate.php
+<form method="post" action="authenticate.php">
+ <input placeholder="email" name="email" type="email"/>
+  <input placeholder="password" name="password" type="text"/>
+   
+	<input type="submit">
+</form>
+Form for update.php
+<form method="post" action="update.php">
+  <input placeholder="pic" name="pic" type="text"/>
+   <input placeholder="update" name="update" type="text"/>
+	<input type="submit" name="submit">
+</form>
+</body>
+
+
+</html>
